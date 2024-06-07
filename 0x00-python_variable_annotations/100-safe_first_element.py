bf6f0cae1278fee405c8389a161f7adf025f09aa
Pibo Shongwe <piboman@yahoo.com>
2024-06-07 04:00:44 +0000

    update

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+100-safe_first_element.py
+"""
+from typing import Sequence, Any, Union
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """Return the first element of the list if it exists, otherwise return None."""
+    if lst:
+        return lst[0]
+    else:
+        return None
