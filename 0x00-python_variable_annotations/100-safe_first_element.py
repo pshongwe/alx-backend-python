56eff9ba25b78fd34c7284d4fc9b102cf7a0b230
Pibo Shongwe <piboman@yahoo.com>
2024-06-07 04:08:12 +0000

    update

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -6,7 +6,8 @@ from typing import Sequence, Any, Union
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
-    """Return the first element of the list if it exists, otherwise return None."""
+    """Return the first element of the list if
+    it exists, otherwise return None."""
     if lst:
         return lst[0]
     else:
--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -8,4 +8,3 @@ from typing import List, Tuple, Iterable, Sequence
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """Return a list of tuples, each containing a sequence and its length."""
     return [(i, len(i)) for i in lst]
-
