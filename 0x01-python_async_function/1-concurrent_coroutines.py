9ed3345258d84a38adaeaeb213455a130b581f5a
Pibo Shongwe <piboman@yahoo.com>
2024-06-11 03:08:32 +0000

    update

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,3 +2,23 @@
 """
 1-concurrent_coroutines.py
 """
+from typing import List
+import asyncio
+from 0_basic_async_syntax import wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """wait n times"""
+    delays = []
+    for _ in range(n):
+        delays.append(await wait_random(max_delay))
+
+    for i in range(1, len(delays)):
+        key = delays[i]
+        j = i - 1
+        while j >= 0 and key < delays[j]:
+            delays[j + 1] = delays[j]
+            j -= 1
+        delays[j + 1] = key
+
+    return delays
