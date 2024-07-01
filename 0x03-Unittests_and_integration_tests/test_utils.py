e7b91ecc532458a69e1fcff4c544795fe6e4c9c7
Pibo Shongwe <piboman@yahoo.com>
2024-07-01 22:24:11 +0000

    update

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,8 +1,39 @@
 #!/usr/bin/env python3
 """unit tests"""
 import unittest
+from unittest.mock import patch, Mock
+from typing import Dict
 from parameterized import parameterized
 from utils import access_nested_map
+from utils import get_json
+
+
+class TestGetJson(unittest.TestCase):
+    """
+    Test case for the get_json function in the utils module.
+    """
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    @patch('utils.requests.get')
+    def test_get_json(self, test_url, test_payload, mock_get):
+        """
+        Test that get_json returns the expected
+        result and makes the correct HTTP call.
+        Parameters:
+        test_url (str): The URL to pass to get_json.
+        test_payload (dict): The expected JSON payload
+        to be returned by get_json.
+        mock_get (Mock): The mocked requests.get method.
+        """
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+        mock_get.return_value = mock_response
+        result = get_json(test_url)
+        mock_get.assert_called_once_with(test_url)
+        self.assertEqual(result, test_payload)
 
 
 class TestAccessNestedMap(unittest.TestCase):
