ec7e4f8ee77658095b1ca266bf5d18e4fd708c09
Pibo Shongwe <piboman@yahoo.com>
2024-07-02 21:49:47 +0000

    update

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -57,5 +57,34 @@ class TestAccessNestedMap(unittest.TestCase):
             access_nested_map(nested_map, path)
 
 
+class TestMemoize(unittest.TestCase):
+    """
+    Test case for the memoize decorator in the utils module.
+    """
+    def test_memoize(self):
+        """
+        Test that the memoize decorator caches the result of the method and
+        calls the method only once.
+        """
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        test_instance = TestClass()
+
+        with patch.object(TestClass,
+                          'a_method',
+                          return_value=42) as mock_method:
+            first_call = test_instance.a_property
+            second_call = test_instance.a_property
+            self.assertEqual(first_call, 42)
+            self.assertEqual(second_call, 42)
+            mock_method.assert_called_once()
+
+
 if __name__ == "__main__":
     unittest.main()
